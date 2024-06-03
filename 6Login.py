aeef6fdff233cbeff2d1c463e946d98a7546145a
basilis9 <baskoletsis@gmail.com>
2024-06-03 18:24:41 +0300

    Add files via upload

---
+++ 6Login.py
@@ -0,0 +1,7 @@
+email = input("Enter your email")
+password = input("Enter your password")
+
+if email == "gvasfw@gmail.com" and password == "gvasfw" :
+    print("Welcome")
+else :
+    print("Try again")
